3cd94e81c0768967a369eb5989e9098971cb0dad
Nick Palenchar <nick@nyllabs.tech>
2017-04-06 10:28:00 -0400

    fix typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports = function(amount, code){
 
 	let acceptable = ["ca", "mx", "eu"];
-	let multiplierIndex = [1.3, 1.6. 0.4]
+	let multiplierIndex = [1.3, 1.6, 0.4]
 
 	if(!acceptable.includes(code)){
 		throw new TypeError("[any-currency] unknown code or improperly formatted code provided");
