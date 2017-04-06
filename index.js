0d3fb84b9690bba649b1aef4a8f81782462b235e
Nick Palenchar <nick@nyllabs.tech>
2017-04-06 10:16:48 -0400

    script

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = function(amount, code){
+
+	let acceptable = ["ca", "mx", "eu"];
+	let multiplierIndex = [1.3, 1.6. 0.4]
+
+	if(!acceptable.includes(code)){
+		throw new TypeError("[any-currency] unknown code or improperly formatted code provided");
+	 }
+
+	return amount * multiplierIndex[acceptable.indexOf(code)];
+
+
+}
